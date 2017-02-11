77bba280996db126ad417dc43fbd4c91e09e24e2
akodate <alex.kodate@gmail.com>
2017-02-10 21:55:12 -0500

    Add showing_class snippets

--- python/showing_class/primary_school.py
+++ python/showing_class/primary_school.py
@@ -6,10 +6,9 @@ class Rectangle(object):
     def __str__(self):
         return '{} x {} = {}'.format(self.height, self.width, self.area)
 
-    def __init__(self, height, width):
-        self.height = height
-        self.width = width
-        self.area = height * width
+    @property
+    def area(self):
+        return self.height * self.width
 
 
 def primarySchool(height, width):
